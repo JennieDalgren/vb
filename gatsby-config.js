462abd8df79f2d89ee6b0d5b7ddc02828de75b7f
Jennie Dalgren <jennie.dalgren@gmail.com>
2018-12-06 23:37:31 +0800

    set up basic blog list and individual blogpost

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,15 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `markdown-pages`,
+        path: `${__dirname}/content`,
       },
+      // {
+      //   name: `images`,
+      //   path: `${__dirname}/src/images`,
+      // },
     },
+    `gatsby-transformer-remark`,
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,55 @@
 import React from 'react'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const WorkPage = () => (
-  <Layout>
-    <div
-      className="wrapper"
-      style={{
-        position: 'fixed',
-        backgroundImage: 'none',
-      }}
-    >
-      <div className="section" style={{ textAlign: 'center' }}>
-        <h1>Here is my bloggy blog blog</h1>
+const BlogPage = props => {
+  console.log('props', props)
+  const postList = props.data.allMarkdownRemark
+  return (
+    <Layout>
+      <div
+        className="wrapper"
+        style={{
+          backgroundImage: 'none',
+        }}
+      >
+        <div className="section" style={{ textAlign: 'center' }}>
+          <h1>Here is my bloggy blog blog</h1>
+          <div className="post-list">
+            {postList.edges.map(({ node }, i) => (
+              <Link key={i} to={node.fields.slug} className="link">
+                <div className="post">
+                  <h4>{node.frontmatter.title}</h4>
+                  <p className="post-date">{node.frontmatter.date}</p>
+                  <p>{node.excerpt}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
-export default WorkPage
+export default BlogPage
+
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM Do YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
