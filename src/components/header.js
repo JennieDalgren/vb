8aeccce16c545cd24c915203e2f6ae127e79b393
Jennie Dalgren <jennie.dalgren@gmail.com>
2018-12-06 20:48:19 +0800

    start setting up header and index page

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,38 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
+import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const activeStyle = {
+  paddingBottom: 10,
+  borderBottom: '2px solid black',
+}
+
+const Header = () => (
+  <div className="header">
+    <div className="menuWrapper">
+      <ul className="menu">
+        <li>
+          <Link to="/" activeStyle={activeStyle}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/work" activeStyle={activeStyle}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" activeStyle={activeStyle}>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="/blog" activeStyle={activeStyle}>
+            Blog
+          </Link>
+        </li>
+      </ul>
     </div>
   </div>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: '',
-}
 
 export default Header
