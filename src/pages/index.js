da356bb7dd6a9b0d82f7509bbc6cedee073485f0
Jennie Dalgren <jennie.dalgren@gmail.com>
2019-01-12 21:15:02 +0700

    styling index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,21 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <div className="wrapper" style={{ position: 'fixed' }}>
-      <div className="section" style={{ textAlign: 'center' }}>
-        <h1>Victoria Bätz</h1>
-
-        <p>I am a bla bla bla bla.</p>
-        <p>Now go read y blog....</p>
+    <div
+      className="wrapper"
+      style={{
+        flexDirection: 'column',
+        position: 'fixed',
+        alignItems: 'center',
+        backgroundImage: `url("/assets/img_8339.jpg")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+      }}
+    >
+      <div className="index-section">
+        <h1>Bienveindo!</h1>
+        <h3>I'm Victoria. </h3>
+        <h4>Storyteller, food lover and culture intermediary/mediator</h4>
       </div>
     </div>
   </Layout>
